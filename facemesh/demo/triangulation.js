fbcd1e338f9248036ef9e7030133ff21d9ea6592
Khushi Bhansali <kbrocks01@gmail.com>
2020-10-07 06:08:51 -0400

    Fixed ReadMe and Worked On Issues git commit -m"Fixed typo"

---
+++ facemesh/demo/triangulation.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+/*Specific coordinates that point to lips and eyes on face*/
+
+export const LIPS = [61,185, 40, 39, 37, 0, 267, 269, 270, 409, 291, 375, 321, 405, 314, 17, 84, 181, 91, 146, 61, 62,78,191,80,81,82, 13, 312, 311, 310, 415, 308,324,318, 402, 317, 14, 87, 178, 88, 95];
+
+
+export const RIGHT_EYE = [414, 286, 258, 257, 259, 260, 467, 359, 255, 339, 254, 253, 252, 256, 341, 463, 362,398, 384, 385, 386, 387, 388, 466, 263, 249, 390, 373, 374, 380, 381, 382];
+
+export const LEFT_EYE = [130,247,30,29,27,28,56, 190, 243, 112, 26, 22, 24, 110, 25, 130, 246, 161, 160, 159, 158, 157, 173, 133, 155, 154, 153, 145, 144, 163, 7];
