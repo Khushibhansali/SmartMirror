df8d692f6b60119e31d747f1219f0c9d23a4c1d6
Khushibhansali <30347957+Khushibhansali@users.noreply.github.com>
2020-12-04 12:29:16 -0500

    Update index.js

--- facemesh/demo/index.js
+++ facemesh/demo/index.js
@@ -23,7 +23,12 @@ import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
 // https://github.com/tensorflow/tfjs/pull/2819 is merged.
 import {version} from '@tensorflow/tfjs-backend-wasm/dist/version';
 
-import {RIGHT_EYE, LEFT_EYE, LIPS} from './triangulation';
+import {RIGHT_EYE, LEFT_EYE, LIPS, BOX_LIPS, ALL_LIPS_POINTS} from './triangulation';
+import { abs } from 'mathjs';
+
+//width and height of boxed lips for image 
+var width;
+var height;
 
 tfjsWasm.setWasmPath(
     `https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-wasm@${
@@ -36,7 +41,6 @@ function isMobile() {
 }
 
 function drawPath(ctx, points, closePath) {
-    console.log(points)
     const region = new Path2D();
     region.moveTo(points[0][0], points[0][1]);
 
@@ -132,28 +136,76 @@ async function renderPrediction() {
         var lip = document.getElementById("lip");
         var right = document.getElementById("right_eye");
         var left = document.getElementById("left_eye");
-        
-	//uses draw path to outline the array of points from triangulation.js
+        var box_around_lip = document.getElementById("box_lips");
+        var all_lip_points = document.getElementById("all_lips");
+
+	      //uses draw path to outline the array of points from triangulation.js
         drawPath(ctx, RIGHT_EYE.map(index => keypoints[index]),true);
         drawPath(ctx, LEFT_EYE.map(index => keypoints[index]),true);
         drawPath(ctx, LIPS.map(index => keypoints[index]),true);
-   
-     	// commented out because this divides array in 3, but we don't need that 
-   /*    for (let i = 0; i < LIPS.length / 3; i++) {
-          const points = [
-           TRIANGULATION[i * 3], TRIANGULATION[i * 3 + 1] , TRIANGULATION[i * 3 + 2]
-          ].map(index => keypoints[index]);
+        //drawPath(ctx, BOX_LIPS.map(index => keypoints[index]),true);
+       // drawPath(ctx, ALL_LIPS_POINTS.map(index => keypoints[index]),true);
 
-          drawPath(ctx, points, true);
+        //array of tuples (x,y,z)
+        var lip_points = ALL_LIPS_POINTS.map(index => keypoints[index]);
+        var allX = []
+        var allY = []
+
+        //adds all X and Y values to respective arrays
+        for (let i = 1; i < lip_points.length; i++) {
+          const point = lip_points[i];
+          allX.push(point[0]);
+          allY.push(point[1]);
         }
-*/
+
+        var minX = allX[0], maxX =  allX[0], minY =  allY[0], maxY=  allY[0];
+    
+        //finds min and max of each coordinate given array of boxed lips
+        for (let i = 1; i < allX.length; i++) {
+          
+          if(allX[i] < minX){
+            minX = allX[i];
+          }
+          if(allX[i] > maxX){
+            maxX = allX[i];
+          }
+
+          if(allY[i] < minY){
+            minY = allY[i];
+          }
+          if(allY[i] > maxY){
+            maxY = allY[i];
+          }
+        }
+
+        width = Math.abs(maxX - minX);
+        height = Math.abs(maxY - minY);
+
+        // find min and max of lips array 
+        // figure out topright, topleft, bottomleft, bottomright values 
+        var minXminY = [minX, minY];  //top left
+        var minXmaxY = [minX, maxY];  // bottom left
+        var maxXminY = [maxX, minY];  // top right
+        var maxXmaxY = [maxX, maxY];  // bottom right
+
+        //drawpath for that box
+        drawPath(ctx, minXminY,true);
+        drawPath(ctx, minXmaxY,true);
+        drawPath(ctx, maxXminY,true);
+        drawPath(ctx, maxXmaxY,true);
+
+        console.log("all points");
+        console.log(minXminY);
+        console.log(minXmaxY);
+        console.log(maxXminY);
+        console.log(maxXmaxY);
+
       } else {
 	
-
         for (let i = 0; i < keypoints.length; i++) {
           const x = keypoints[i][0];
           const y = keypoints[i][1];
-	   }
+	      }
           ctx.beginPath();
           ctx.arc(x, y, 1 /* radius */, 0, 2 * Math.PI);
           ctx.fill();
@@ -212,6 +264,43 @@ async function main() {
   ctx.strokeStyle = '#32EEDB';
   ctx.lineWidth = 0.5;
 
+
+  $("#submitGraphic").click(function(){
+    var canvas = document.getElementsById("output");
+    // canvas context
+    var context = canvas[0].getContext("2d");
+
+    // get the current ImageData for the canvas - substitue code values 
+    var data = context.getImageData(0, 0, width, height);
+
+    // store the current globalCompositeOperation
+    var compositeOperation = context.globalCompositeOperation;
+
+    // set to draw behind current content
+    context.globalCompositeOperation = "destination-over";
+
+    //set background color
+    context.fillStyle = "#FFFFFF";
+
+    // draw background/rectangle on entire canvas - substitue code values 
+    context.fillRect(0, 0, width, height);  
+    var tempCanvas = document.createElement("canvas"),
+        tCtx = tempCanvas.getContext("2d");
+    
+    tempCanvas.width = 640;
+    tempCanvas.height = 480;
+    
+    tCtx.drawImage(canvas[0],0,0);
+    
+    // write on screen
+    var img = tempCanvas.toDataURL("image/png");
+    document.write('<a href="'+img+'"><img src="'+img+'"/></a>');
+  })
+
+  var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  
+  window.location.href=image; // save locally
+
+  
   model = await facemesh.load({maxFaces: state.maxFaces});
   renderPrediction();
 
